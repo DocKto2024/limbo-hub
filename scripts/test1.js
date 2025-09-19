a5e42e6170f0f50e0e27178328d4e03ecb3a50a6
DocKto2024 <165499846+DocKto2024@users.noreply.github.com>
2025-09-19 18:54:18 +0200

    Update test1.js

--- scripts/test1.js
+++ scripts/test1.js
@@ -5,14 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const wrapper = document.createElement("div");
     wrapper.classList.add("image-wrapper");
 
-    // Создаем overlay с текстом alt
     const overlay = document.createElement("div");
     overlay.classList.add("image-overlay");
     overlay.textContent = img.alt;
 
-    // Вставляем overlay и img в wrapper
     img.parentNode.insertBefore(wrapper, img);
     wrapper.appendChild(img);
     wrapper.appendChild(overlay);
   });
+
 });
